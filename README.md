c96881ba7397177374859092caa085c2b7bf68c6
maximus136 <>
2017-09-08 01:33:07 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # foobar-react
 Hackathon solution in React. Presented by FooBar!
+
+### Clone the Repo
+### npm install or yarn
+#### Use NPM scripts
+
+- `npm run build` to build an optimized version of your application in /dist
+- `npm run serve` to launch a browser sync server on your source files
+- `npm run serve:dist` to launch a server on your optimized application
+- `npm run test` to launch your unit tests with Karma
+- `npm run test:auto` to launch your unit tests with Karma in watch mode
---
+++ conf/webpack.conf.js
@@ -0,0 +1,86 @@
+const webpack = require('webpack');
+const conf = require('./gulp.conf');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FailPlugin = require('webpack-fail-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loaders: [
+          'json-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        options: {
+          emitError: false,
+          emitWarning: true
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot-loader',
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    FailPlugin,
+    new HtmlWebpackPlugin({
+      template: conf.path.src('index.html')
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: () => [autoprefixer]
+      },
+      debug: true
+    })
+  ],
+  devtool: 'source-map',
+  output: {
+    path: path.join(process.cwd(), conf.paths.tmp),
+    filename: 'index.js'
+  },
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    `./${conf.path.src('index')}`
+  ]
+};
