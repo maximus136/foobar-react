68a0d423231faf854950dfaf526309789e8f0dda
Swapnil Dubal <sdubal@deloitte.com>
2017-09-08 16:08:27 +0530

    Added header, button,hero component

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './header.scss';
+import Button from '../button/Button';
 
 class Header extends React.Component {
     render() {
         return(
-            <header>
-                <h1>This is a header component</h1>
+            <header className="justify-content-between">
+                <div className="logo">
+                    <a href="#.">
+                        <img src="/app/assets/logo1.svg" alt="logo" />
+                    </a>
+                </div>
+                <div className="navigation">
+                    <a className="navLink active">HOME</a>
+                    <a className="navLink">ABOUT US</a>
+                    <a className="navLink">ELEMENTS</a>
+                    <a className="navLink">FAQ</a>
+                    <Button classNames="regular-size btn-empty" value="Sign Up"/>
+                </div>
             </header>
         );
     }
